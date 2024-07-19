13fcd3b852c8103942ce9fb8b280c08a9c3c8c75
Vansh <vanshvansh0227@gmail.com>
2024-07-19 13:30:32 +0530

    Day 1 added

---
+++ Day1.js
@@ -0,0 +1,54 @@
+// // Activity 1.1
+// var value =23;
+// console.log(value); // output -23
+
+
+// // Activity 1.2
+// let name = "Vansh";
+// console.log(name); // output- Vansh
+
+// //Activity 2 
+// const flag = true;
+// console.log(flag);//output- true
+
+// // Activity 3
+//     var num =23;
+//     var string1 = "vansh";
+//     var boolval = true;
+//     var array = [1,2,3,4,3];
+//     var object = {
+//         firstname : "Vansh",
+//         lastName : "Gupta"
+//     };
+//     var date = new Date("2024-07-19");
+    
+//     console.log(typeof(num));//number
+//     console.log(typeof(string1));//string
+//     console.log(typeof(boolval));////object
+//     console.log(typeof(array));//object
+//     console.log(typeof(object));//object
+//     console.log(typeof(date));//object
+
+// //Activity 4 
+// let name = "vansh";
+// console.log(name);
+// name = "Gupta";
+// console.log(name);
+
+// Activity 5 
+const name = "vansh";
+console.log(name);
+name = "Gupta";
+console.log(name);
+
+
+// Output -
+
+// TypeError: Assignment to constant variable.
+//     at Object.<anonymous> 
+
+
+
+
+
+
