8d61bd767426803f9f16daff5e4dd6d1a8a267f6
Vansh <vanshvansh0227@gmail.com>
2024-07-20 12:54:48 +0530

    Added Day3.js

---
+++ Day2.js
@@ -0,0 +1,54 @@
+// Activity 1
+console.log(3+5);
+console.log(3-5);
+console.log(3*5);
+console.log(3/5);
+console.log(3%5);
+
+
+// Activity 2 \
+var a = 3;
+a+=2;
+comsole.log(a);
+a-=1;
+console.log(a);
+
+
+// Activity 3
+
+console.log(5>3);
+console.log(5<3);
+
+console.log(5<=3);
+console.log(5>=3);
+
+console.log(3==3);
+console.log(5===3);
+
+
+// Activity 4 
+console.log(5==3 && 5>4);
+console.log(5>=3 || false);
+
+
+console.log(true!=false);
+
+console.log(!true);
+
+
+// Activity 5
+var name = "vansh";
+var a =2;
+const flag = true;
+name = flag?"gupta":"vansh";
+console.log(name);
+
+
+
+
+
+
+
+
+
+
